6785ad21a8268ca57492e9a283a50d67981a7b85
Greg French <gregfrench26@gmail.com>
2021-05-02 11:15:18 -0400

    Added Edge component

---
+++ src/components/Edge.js
@@ -0,0 +1,15 @@
+function Edge(props) {
+  let coords = props.coordinates;
+
+  return (
+    <line
+      x1={coords.x1}
+      y1={coords.y1}
+      x2={coords.x2}
+      y2={coords.y2}
+      className="stroke-current stroke-2 text-black-500"
+    />
+  );
+}
+
+export default Edge;
