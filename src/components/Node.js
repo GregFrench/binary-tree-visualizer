6582d33a74b85aed94a204e3c0620434ae5794c2
Greg French <gregfrench26@gmail.com>
2021-05-02 15:01:23 -0400

    Cleaned up Node.js component code

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,10 +1,27 @@
 function Node(props) {
   let coords = props.coordinates;
+  let radius = props.radius;
 
   return (
     <g>
-      <circle cx={coords.x} cy={coords.y} r="40" stroke="black" stroke-width="3" fill="white" />
-      <text x={coords.x} y={parseInt(coords.y) + 5} text-anchor="middle" fill="black" font-weight="bold">{props.value}</text>
+      <circle
+        cx={coords.x}
+        cy={coords.y}
+        r={radius}
+        stroke="black"
+        stroke-width="3"
+        fill="white"
+      />
+
+      <text
+        x={coords.x}
+        y={parseInt(coords.y) + 5}
+        text-anchor="middle"
+        fill="black"
+        font-weight="bold"
+      >
+        {props.value}
+      </text>
     </g>
   );
 }
