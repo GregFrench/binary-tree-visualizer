bc378c030a97b675c6a685f6ae355fbb062543b8
Greg French <gregfrench26@gmail.com>
2021-05-08 11:25:03 -0400

    Added the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="py-2 bg-green-500">
+      <div className="container mx-auto">
+        <h1 className="text-xl text-white">Binary Tree Visualizer</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
