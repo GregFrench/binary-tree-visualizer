477cb5319f29bcc4ae1df185d386ec65010c943d
Greg French <gregfrench26@gmail.com>
2021-05-15 11:12:47 -0400

    Updated styling for the Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,15 +3,15 @@ function Dropdown(props) {
   let items = props.items;
 
   return (
-    <div className="dropdown float-right pl-6 relative text-white">
-      <div class="dropbtn cursor-pointer p-3">
+    <div className="dropdown float-right relative text-white">
+      <div className="dropbtn cursor-pointer p-3 px-4">
         <span>{title}</span> 
         <span className="pl-3"><i class="fas fa-caret-down"></i></span>
       </div>
-      <div class="dropdown-content bg-green-500 hidden">
+      <div className="dropdown-content absolute bg-green-500 hidden min-w-max right-0 rounded-b-lg shadow-lg z-10">
         {items.map((item) => {
           return (
-            <span className="text-white block cursor-pointer px-4 py-3">{item}</span>
+            <span className="block cursor-pointer hover:bg-green-700 px-4 py-3 text-white">{item}</span>
           );
         })}
       </div>
