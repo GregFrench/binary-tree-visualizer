c36d40597881da246b45f7ad5353fced7183bf8c
Greg French <gregfrench26@gmail.com>
2021-05-29 15:42:09 -0400

    Swtiched to rendering dropdown using composition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React, {useState} from 'react';
 import Header from './components/Header.js';
 import Tree from './components/Tree.js';
+import Dropdown from './components/Dropdown';
+import {algorithmItems, treeItems} from './utils';
 import './App.css';
 
 function App() {
+  const [selectedTreeValue, setSelectedTreeValue] = useState();
+  const [selectedAlgorithmValue, setSelectedAlgorithmValue] = useState();
+
   return (
     <div>
-      <Header></Header>
+      <Header>
+        <Dropdown
+          title={'Tree'}
+          items={treeItems}
+          selectedValue={selectedTreeValue}
+          setSelectedValue={setSelectedTreeValue}
+        ></Dropdown>
+        <Dropdown
+          title={'Algorithm'}
+          items={algorithmItems}
+          selectedValue={selectedAlgorithmValue}
+          setSelectedValue={setSelectedAlgorithmValue}
+        ></Dropdown>
+      </Header>
       <div className="container mx-auto">
         <Tree></Tree>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,9 @@
-import Dropdown from './Dropdown';
-import {algorithmItems, treeItems} from '../utils';
-
-function Header() {
+function Header(props) {
   return (
     <header className="bg-green-500">
       <div className="container mx-auto flex">
         <h1 className="pr-3 py-2 text-xl text-white">Binary Tree Visualizer</h1>
-        <Dropdown title={'Tree'} items={treeItems}></Dropdown>
-        <Dropdown title={'Algorithm'} items={algorithmItems}></Dropdown>
+        {props.children}
       </div>
     </header>
   );
